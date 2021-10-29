b7cb215e7b9e899946791a28c5b8beb75e542fea
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2021-10-29 11:25:17 +0530

    appjs changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 //const cors = require("cors");
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
@@ -29,6 +29,7 @@ const unit = require("./routes/unit");
 const altunit = require("./routes/altunit");
 const privacypolicy = require("./routes/privacypolicy");
 const cart = require("./routes/cart");
+
 //use
 app.use("/", user);
 app.use("/", product);
