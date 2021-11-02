eb7e6dd8db9671f8f2d0268d158a3ea63b9c8708
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2021-11-02 12:35:38 +0530

    few change

--- models/addwishlist.js
+++ models/addwishlist.js
@@ -4,9 +4,6 @@ const AddwishlistSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
     product: { type: mongoose.Schema.Types.ObjectId, ref: "product" },
-    product_price: {
-      type: Number,
-    },
   },
   { timestamps: true }
 );
