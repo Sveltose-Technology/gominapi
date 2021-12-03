2ec8404003d4f3e59094069c8bbc3424780e36ac
rishabh <rishabhtiwari3060@gmail.com>
2021-12-03 13:29:02 +0530

    commit first

--- routes/termsandcondition.js
+++ routes/termsandcondition.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const {
-  addtermsandcondition,
-  deltermsandcondition,
-  alltermsandcondition,
-} = require("../controller/termsandcondition");
+// const {
+//   addtermsandcondition,
+//   deltermsandcondition,
+//   alltermsandcondition,
+// } = require("../controller/termsandcondition");
 
-router.post("/admin/addtermsandcondition", addtermsandcondition);
-// router.post("/admin/editunits/:id", editunits);
-// router.get("/admin/viewoneunits/:id", viewoneunits);
-router.get("/admin/get_termsandcondition", alltermsandcondition);
-router.get("/admin/deltermsandcondition/:id", deltermsandcondition);
+// router.post("/admin/addtermsandcondition", addtermsandcondition);
+// // router.post("/admin/editunits/:id", editunits);
+// // router.get("/admin/viewoneunits/:id", viewoneunits);
+// router.get("/admin/get_termsandcondition", alltermsandcondition);
+// router.get("/admin/deltermsandcondition/:id", deltermsandcondition);
 
-module.exports = router;
+// module.exports = router;
