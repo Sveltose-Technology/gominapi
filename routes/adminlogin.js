601a5128b8f534c3a43e4674d0ecf9f95e107d5c
rishabh <rishabhtiwari3060@gmail.com>
2021-12-06 15:22:46 +0530

    commit first

--- routes/adminlogin.js
+++ routes/adminlogin.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    adminlogin,
+    adminlogin,createadmin
 } = require("../controller/adminlogin");
 
 //Paths
+router.post("/admin/createadmin", createadmin);
 router.post("/admin/adminlogin", adminlogin);
  
 module.exports = router;
