fc8240cb030e852fbec052533bf3aaf2991c3684
rishabh <rishabhtiwari3060@gmail.com>
2021-12-06 17:43:26 +0530

    commit one

--- routes/subscription.js
+++ routes/subscription.js
@@ -7,7 +7,7 @@ const {addSubscription,Getsubscription,getoneSubscription,del_subscription
 
 router.post("/admin/addSubscription", addSubscription);
 router.get("/admin/Getsubscription", Getsubscription);
-router.get("/admin/getoneSubscription", getoneSubscription);
-router.get("/admin/del_subscription", del_subscription);
+router.get("/admin/getoneSubscription/:id", getoneSubscription);
+router.get("/admin/del_subscription/:id", del_subscription);
 
 module.exports = router;
