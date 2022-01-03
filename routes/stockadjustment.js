ac856748880b00b1a3b1d20922fa8702dd6f4dc5
rishabh <rishabhtiwari3060@gmail.com>
2022-01-03 19:00:58 +0530

    commit first

--- routes/stockadjustment.js
+++ routes/stockadjustment.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { addstockadjustment,delstockadjustment } = require("../controller/stockadjustment");
+const { addstockadjustment,delstockadjustment,getstockadjustment } = require("../controller/stockadjustment");
 
 //Paths
 router.post("/admin/addstockadjustment", addstockadjustment);
 router.get("/admin/delstockadjustment/:id", delstockadjustment);
+router.get("/admin/getstockadjustment", getstockadjustment);
+
 
  
 module.exports = router;
