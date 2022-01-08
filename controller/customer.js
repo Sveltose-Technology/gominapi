bc2edd6177ccaf21f5026892a214276ef8929858
rishabh <rishabhtiwari3060@gmail.com>
2022-01-08 18:01:46 +0530

    done

--- controller/customer.js
+++ controller/customer.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const nodemailer = require("nodemailer")
+const {sendmail} = require("./mail");
+
+const router = require("../routes/mail");
 
 const validatePassword = (password, dbpassword) => {
   bcrypt.compareSync(password, dbpassword);
@@ -205,12 +208,47 @@ exports.totalcustomer = async(req,res) =>{
 
 
 
-const defaultotp = 1234;
 exports.sendotp = async (req, res) => {
+  const defaultotp = Math.ceil(Math.random()*999999);
   const { mobile_no } = req.body;
   const finddetails  = await Customer.findOne({mobile_no : mobile_no})
   //console.log(mobile_no.length);
+  //console.log(finddetails);
+  //console.log(finddetails.customer_email);
   if (finddetails) {
+    //const {to,text,} = req.body
+    const subject = `Buynaa Email Verification`;
+    const text = `<h4>Your verfication code is ${defaultotp}</h4>`;
+
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtpout.secureserver.net",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "support@buynaa.com", // generated ethereal user
+      pass: "Buynaa330*", // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Buynaa Support" <support@buynaa.com>', // sender address
+    to: finddetails.customer_email, // list of receivers
+    subject: subject, // Subject line
+    text: text, // plain text body
+    html: `<b>${text}</b>`, // html body
+  })
+
+  console.log("Message sent: %s", info);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     res.status(200).json({
       status: true,
       msg: "otp send successfully",
