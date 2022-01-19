15f296ba314cef1489421dd553487d5780260801
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-01-19 10:59:36 +0530

    commit

--- routes/user_address.js
+++ routes/user_address.js
@@ -14,6 +14,6 @@ const {
 router.post("/user/addcus_address",verifytoken, addcus_address);
 router.post("/user/edit_address", verifytoken, edit_address);
 router.get("/user/getaddress", verifytoken, getaddress);
-router.post("/user/viewoneuseraddress", verifytoken, viewoneuseraddress);
+router.get("/user/viewoneuseraddress", verifytoken, viewoneuseraddress);
 
 module.exports = router;
