68600a4060fdd24096ba7c4ac542674a21d48580
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-01-25 16:46:14 +0530

    commit first

--- routes/adminlogin.js
+++ routes/adminlogin.js
@@ -1,15 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const fs = require("fs");
 
 const {
-    adminlogin,createadmin,editadmin,getoneadmin
+  adminlogin,
+  createadmin,
+  editadmin,
+  getoneadmin,
 } = require("../controller/adminlogin");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let path = `./uploadesimages`;
+    if (!fs.existsSync("uploadesimages")) {
+      fs.mkdirSync("uploadesimages");
+    }
+    cb(null, path);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype.includes("jpeg") ||
+    file.mimetype.includes("png") ||
+    file.mimetype.includes("jpg")
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+let uploads = multer({ storage: storage });
+
 //Paths
-router.post("/admin/createadmin", createadmin);
+router.post("/admin/createadmin", uploads.single("image"), createadmin);
 router.post("/admin/adminlogin", adminlogin);
 router.post("/admin/editadmin/:id", editadmin);
 router.get("/admin/getoneadmin/:id", getoneadmin);
 
- 
 module.exports = router;
