2599a0ce36ab3b80f086036ea1246588e9afa392
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2022-04-28 17:02:34 +0530

    commit

--- controller/adminlogin.js
+++ controller/adminlogin.js
@@ -183,10 +183,9 @@ exports.getoneadmin = async (req, res) => {
 };
 
 
-
 exports.sendotp = async (req, res) => {
   const defaultotp = Math.ceil(100000 + Math.random() * 900000);
-  const { mobile,email } = req.body;
+  const { mobile } = req.body;
   const http = require("https");
   // const finddetails = await Seller.findOneAndUpdate(
   //   { mobile: mobile },
@@ -224,9 +223,7 @@ exports.sendotp = async (req, res) => {
 
     requestmain.end();
     const finddetails = await Seller.findOneAndUpdate(
-      {
-        $or: [{ mobile: mobile }, { email: email }],
-      },
+      { mobile: mobile },
       { $set: { otp: defaultotp } },
       { new: true }
     );
@@ -237,29 +234,29 @@ exports.sendotp = async (req, res) => {
 
     //Generate test SMTP service account from ethereal.email
     //Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
+    //let testAccount = await nodemailer.createTestAccount();
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtpout.secureserver.net",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: "support@brizebond.com", // generated ethereal user
-        pass: "Buynaa@02771", // generated ethereal password
-      },
-    });
+    // // create reusable transporter object using the default SMTP transport
+    // let transporter = nodemailer.createTransport({
+    //   host: "smtpout.secureserver.net",
+    //   port: 587,
+    //   secure: false, // true for 465, false for other ports
+    //   auth: {
+    //     user: "support@brizebond.com", // generated ethereal user
+    //     pass: "Buynaa@02771", // generated ethereal password
+    //   },
+    // });
 
     // // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Buynaa Support" <support@buynaa.com>', // sender address
-      to: finddetails.email, // list of receivers
-      subject: subject, // Subject line
-      text: text, // plain text body
-      html: `<b>${text}</b>`, // html body
-    })
+    // let info = await transporter.sendMail({
+    //   from: '"Buynaa Support" <support@buynaa.com>', // sender address
+    //   to: finddetails.email, // list of receivers
+    //   subject: subject, // Subject line
+    //   text: text, // plain text body
+    //   html: `<b>${text}</b>`, // html body
+    // })
 
-     console.log("Message sent: %s", info);
+    //  console.log("Message sent: %s", info);
     // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
     // // Preview only available when sending through an Ethereal account
